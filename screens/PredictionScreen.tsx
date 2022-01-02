b9cdfedff2b372f64631538050d673690d5bdc83
Matthew Skillington <matthewskillington14@gmail.com>
2022-01-02 17:35:07 +0000

    feature: connect aws lambda api

--- screens/PredictionScreen.tsx
+++ screens/PredictionScreen.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {
   StyleSheet, View,
 } from 'react-native';
 import { Text } from '../components/Themed';
+import { usePredictionData } from '../hooks/usePredictionData';
 
 import { RootStackScreenProps } from '../types';
 
@@ -18,9 +20,17 @@ const styles = StyleSheet.create({
 
 export default function PredictionScreen({ route }: RootStackScreenProps<'Prediction'>) {
   const { symbol } = route.params;
+  const [stock] = useState(symbol);
+  const { data } = usePredictionData(stock);
+
+  
   return (
     <View style={styles.container}>
       <Text style={styles.symbolText}>{symbol}</Text>
+      {!!data ?
+        <Text>{JSON.stringify(data)}</Text> :
+        <Text>Loading...</Text>
+      }
     </View>
   );
 }
