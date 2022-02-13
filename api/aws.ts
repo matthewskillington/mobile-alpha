d4002de6213400dc9b29b9f0baa7befacaf50101
Matthew Skillington <matthewskillington14@gmail.com>
2022-02-13 16:31:26 +0000

    fix: Support London Stock Exchange on predictions

--- api/aws.ts
+++ api/aws.ts
@@ -3,7 +3,12 @@ import { AWS_GATEWAY_ENDPOINT, AWS_API_KEY } from '@env';
 const BASE_URL = AWS_GATEWAY_ENDPOINT;
 
 const getPrediction = async (symbol: string) => {
-  const response = await fetch(`${BASE_URL}?symbol=${symbol}`, {
+  // Yahoo finance uses a different format to identify stocks on the LSE
+  let reformattedString = symbol;
+  if (symbol.substr(symbol.length - 4) === '.LON') {
+    reformattedString = `${symbol.slice(0, symbol.length - 4)}.L`;
+  }
+  const response = await fetch(`${BASE_URL}?symbol=${reformattedString}`, {
     method: 'GET',
     headers: { 'X-API-KEY': AWS_API_KEY },
   });
